2c5bcaa52561bf194dc59c7d7df1d566ebdfff49
dhineshdhinesh2007 <dhineshpalanisamy4@gmail.com>
2026-03-25 20:25:56 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,25 @@
+
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Choose operation: +, -, *, /")
+choice = input("Enter choice: ")
+
+if choice == '+':
+    print("Result:", num1 + num2)
+
+elif choice == '-':
+    print("Result:", num1 - num2)
+
+elif choice == '*':
+    print("Result:", num1 * num2)
+
+elif choice == '/':
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Cannot divide by zero")
+
+else:
+    print("Invalid choice")
